a47523022a55b4298400786764946e9aa1f12447
Katie Byers <lobsterkatie@gmail.com>
2021-04-28 14:57:16 -0700

    check if project has npm-run-all installed, list SDKs installed deps

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,8 +1,10 @@
+yarn list --pattern npm-run-all
 echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
 cd node_modules/@sentry/nextjs
 echo "INSTALLING SDK DEPENDENCIES"
 yarn
-yarn list --pattern npm-run-all
+yarn list --depth=0
+# yarn list --pattern npm-run-all
 echo "BUILDING SDK"
 echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
 yarn build
