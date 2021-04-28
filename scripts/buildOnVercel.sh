bc594c0e71c10d4ee25bcef5c4207f002de6fc58
Katie Byers <lobsterkatie@gmail.com>
2021-04-28 14:46:36 -0700

    add log statements to vercel build script

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,6 +1,12 @@
+echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
 cd node_modules/@sentry/nextjs
+echo "INSTALLING SDK DEPENDENCIES"
 yarn
+echo "BUILDING SDK"
+echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
 yarn build
 echo "NEXTJS SDK BUILT SUCCESSFULLY"
+echo "RETURNING TO PROJECT ROOT"
 cd ../../..
+echo "BUILDING PROJECT"
 yarn build
