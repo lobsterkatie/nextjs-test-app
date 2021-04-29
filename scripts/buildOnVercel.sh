422eae48834df07a766c7a2241fe88417c08c0e3
Katie Byers <lobsterkatie@gmail.com>
2021-04-28 18:26:39 -0700

    force dev dependencies to be installed

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,12 +1,13 @@
-yarn list --pattern npm-run-all
+# yarn list --pattern npm-run-all
 echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
 cd node_modules/@sentry/nextjs
-cat package.json
+# cat package.json
 echo "INSTALLING SDK DEPENDENCIES"
-yarn
+# this makes it install dev dependencies, which we need for building
+yarn --prod false
 yarn list --depth=0
-yarn add npm-run-all
-yarn list --depth=0
+# yarn add npm-run-all
+# yarn list --depth=0
 # yarn list --pattern npm-run-all
 echo "BUILDING SDK"
 echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
