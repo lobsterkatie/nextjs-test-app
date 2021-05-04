2d144b06aa1b930c67044e4dfef7523425e59780
Katie Byers <lobsterkatie@gmail.com>
2021-05-04 06:40:49 -0700

    show package.json from SDK

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,7 +1,7 @@
 # yarn list --pattern npm-run-all
 echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
 cd node_modules/@sentry/nextjs
-# cat package.json
+cat package.json
 echo "INSTALLING SDK DEPENDENCIES"
 # this makes it install dev dependencies, which we need for building
 yarn --prod false
