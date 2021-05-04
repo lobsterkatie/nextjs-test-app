4fc068f25a8875dd5798e378541c2523e16e05fc
Katie Byers <lobsterkatie@gmail.com>
2021-05-04 06:46:32 -0700

    change to trigger redeploy

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,6 +1,7 @@
 # yarn list --pattern npm-run-all
 echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
 cd node_modules/@sentry/nextjs
+# a
 cat package.json
 echo "INSTALLING SDK DEPENDENCIES"
 # this makes it install dev dependencies, which we need for building
