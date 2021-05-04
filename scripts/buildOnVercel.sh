85ec1b4432052f2e0b6ca33d64aaa2fb1feff8ac
Katie Byers <lobsterkatie@gmail.com>
2021-05-04 09:58:28 -0700

    build other sentry packages first

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -2,7 +2,7 @@
 echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
 cd node_modules/@sentry/nextjs
 # a
-cat package.json
+# cat package.json
 echo "INSTALLING SDK DEPENDENCIES"
 # this makes it install dev dependencies, which we need for building
 yarn --prod false
@@ -10,6 +10,16 @@ yarn list --depth=0
 # yarn add npm-run-all
 # yarn list --depth=0
 # yarn list --pattern npm-run-all
+for package in ("types" "utils" "hub" "core", "minimal" "browser" "node" "react" "integrations"); do
+    # ${var-name:u} converts to uppercase in zsh (can also do ${(U)var-name},
+    # and the same works with l/L for lowercase)
+    echo "\n\n***** @SENTRY/${package:u} *****\n"
+    cd node_modules/${package}
+    # echo "\n Deleting node_modules\n"
+    # yarn
+    yarn build
+    cd -
+done
 echo "BUILDING SDK"
 echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
 yarn build
