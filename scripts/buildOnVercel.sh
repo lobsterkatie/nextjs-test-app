bff989ae76fb040b2b9c3c30bb28a8fa5f38818c
Katie Byers <lobsterkatie@gmail.com>
2021-05-12 03:41:47 -0700

    run why on @sentry/types

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,9 +1,9 @@
 # yarn list --pattern npm-run-all
-echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
+echo -e "\nCD-ING INTO NODE_MODULES/@SENTRY/NEXTJS\n"
 cd node_modules/@sentry/nextjs
 # a
 # cat package.json
-echo "INSTALLING SDK DEPENDENCIES"
+echo $'\nINSTALLING SDK DEPENDENCIES\n'
 # this makes it install dev dependencies, which we need for building
 yarn --prod false
 ls -l node_modules/@sentry
@@ -14,7 +14,9 @@ ls -l node_modules/@sentry
 for package in "types" "utils" "hub" "minimal" "core" "browser" "tracing" "node" "react" "integrations"; do
   # ${var-name:u} converts to uppercase in zsh (can also do ${(U)var-name},
   # and the same works with l/L for lowercase)
-  echo "\n\n***** @SENTRY/${package:u} *****\n"
+  echo
+  echo
+  echo "***** @SENTRY/${package:u} *****\n"
   cd node_modules/@sentry/${package}
   # echo "\n Deleting node_modules\n"
   # yarn
@@ -22,6 +24,8 @@ for package in "types" "utils" "hub" "minimal" "core" "browser" "tracing" "node"
   yarn build
   cd -
 done
+yarn list --pattern \@sentry
+yarn why \@sentry/types
 echo "BUILDING SDK"
 echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
 yarn build
