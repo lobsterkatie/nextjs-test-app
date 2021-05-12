7b7fdef76bec7c42bd72cf6c49edfd530e1ccf7f
Katie Byers <lobsterkatie@gmail.com>
2021-05-11 18:17:15 -0700

    git rid of stray comma

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -11,7 +11,7 @@ ls -l node_modules/@sentry
 # yarn add npm-run-all
 # yarn list --depth=0
 # yarn list --pattern npm-run-all
-for package in "types" "utils" "hub" "core", "minimal" "browser" "node" "react" "integrations"; do
+for package in "types" "utils" "hub" "core" "minimal" "browser" "node" "react" "integrations"; do
   # ${var-name:u} converts to uppercase in zsh (can also do ${(U)var-name},
   # and the same works with l/L for lowercase)
   echo "\n\n***** @SENTRY/${package:u} *****\n"
