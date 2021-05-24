a21360255b71a0f3ace26241d0bef240417d38ce
Katie Byers <lobsterkatie@gmail.com>
2021-05-24 16:19:38 -0700

    turn off script for use with gitpkg

--- scripts/buildOnVercel.sh
+++ scripts/buildOnVercel.sh
@@ -1,53 +1,53 @@
-# yarn list --pattern npm-run-all
-yarn list --pattern \@types
-echo "  "
-echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
-echo "  "
-cd node_modules/@sentry/nextjs
-# a
-# cat package.json
-echo "  "
-echo "INSTALLING SDK DEPENDENCIES"
-echo "  "
-# this makes it install dev dependencies, which we need for building
-yarn --prod false
-ls -l node_modules/@sentry
-yarn list --pattern \@types
-yarn list --depth=0
-# yarn add npm-run-all
+# # yarn list --pattern npm-run-all
+# yarn list --pattern \@types
+# echo "  "
+# echo "CD-ING INTO NODE_MODULES/@SENTRY/NEXTJS"
+# echo "  "
+# cd node_modules/@sentry/nextjs
+# # a
+# # cat package.json
+# echo "  "
+# echo "INSTALLING SDK DEPENDENCIES"
+# echo "  "
+# # this makes it install dev dependencies, which we need for building
+# yarn --prod false
+# ls -l node_modules/@sentry
+# yarn list --pattern \@types
 # yarn list --depth=0
-# yarn list --pattern npm-run-all
+# # yarn add npm-run-all
+# # yarn list --depth=0
+# # yarn list --pattern npm-run-all
 
-for package in "types" "utils" "hub" "minimal" "core" "browser" "tracing" "node" "react" "integrations"; do
-  # ${var-name:u} converts to uppercase in zsh (can also do ${(U)var-name},
-  # and the same works with l/L for lowercase)
-  echo "  "
-  echo "***** @SENTRY/${package:u} *****"
-  echo "  "
-  cd node_modules/@sentry/${package}
-  # echo "\n Deleting node_modules\n"
-  # yarn
-  yarn --prod false
-  # yarn build
-  cd -
-done
+# for package in "types" "utils" "hub" "minimal" "core" "browser" "tracing" "node" "react" "integrations"; do
+#   # ${var-name:u} converts to uppercase in zsh (can also do ${(U)var-name},
+#   # and the same works with l/L for lowercase)
+#   echo "  "
+#   echo "***** @SENTRY/${package:u} *****"
+#   echo "  "
+#   cd node_modules/@sentry/${package}
+#   # echo "\n Deleting node_modules\n"
+#   # yarn
+#   yarn --prod false
+#   # yarn build
+#   cd -
+# done
 
-yarn list --pattern \@types
-yarn why \@types/cookie
-ls -l node_modules/@sentry
-# ls -l node_modules/@sentry/react
-# ls -l node_modules/@sentry/react/
-ls -l node_modules/@sentry/node/node_modules/@sentry
-ls -l node_modules/@sentry/node/node_modules/@types
-echo "  "
-echo "BUILDING SDK"
-echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
-echo "  "
-yarn build
-echo "NEXTJS SDK BUILT SUCCESSFULLY"
-echo "RETURNING TO PROJECT ROOT"
-cd ../../..
-echo "  "
-echo "BUILDING PROJECT"
-echo "  "
-yarn build
+# yarn list --pattern \@types
+# yarn why \@types/cookie
+# ls -l node_modules/@sentry
+# # ls -l node_modules/@sentry/react
+# # ls -l node_modules/@sentry/react/
+# ls -l node_modules/@sentry/node/node_modules/@sentry
+# ls -l node_modules/@sentry/node/node_modules/@types
+# echo "  "
+# echo "BUILDING SDK"
+# echo "(Hint: We need to do this because we're installing it straight from a GH branch, not from npm.)"
+# echo "  "
+# yarn build
+# echo "NEXTJS SDK BUILT SUCCESSFULLY"
+# echo "RETURNING TO PROJECT ROOT"
+# cd ../../..
+# echo "  "
+# echo "BUILDING PROJECT"
+# echo "  "
+# yarn build
