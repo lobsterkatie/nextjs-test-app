a464f0e96a6704640e48deacdb96e1048677ef8f
Katie Byers <lobsterkatie@gmail.com>
2021-06-04 14:47:01 -0700

    ls fewer things

--- .sentry/TEST.sh
+++ .sentry/TEST.sh
@@ -1,9 +1,9 @@
-echo "  root"
-ls -a
-echo "  .next"
-ls -a .next
-echo "  .next/server"
-ls -a .next/server
+# echo "  root"
+# ls -a
+# echo "  .next"
+# ls -a .next
+# echo "  .next/server"
+# ls -a .next/server
 mv .next/analyze/* public
 cp .next/server/webpack-runtime* public
 
