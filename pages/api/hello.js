3f234926cb983972ba74a2568a8eeb984510de17
Katie Byers <lobsterkatie@gmail.com>
2021-07-06 12:46:16 -0700

    wait 5 sec in handler

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,11 +4,18 @@ import * as Sentry from "@sentry/nextjs";
 Error.stackTraceLimit = Infinity;
 
 async function stuff() {
+  let counter = 5;
+  while (counter > 0) {
+    setTimeout(() => {
+      counter -= 1;
+      console.log("new counter value:", counter);
+    }, 1000);
+  }
   console.log("I'm in the stuff function");
 }
 
 const handler = async (req, res) => {
-  setImmediate(stuff);
+  // setImmediate(stuff);
   console.log(
     "\nI'm in the api route. Client defined:",
     Sentry.getCurrentHub().getClient() !== undefined
@@ -17,7 +24,7 @@ const handler = async (req, res) => {
   Sentry.captureException(new Error("in /hello API route"));
   // console.log("stack", new Error("").stack);
   await fetch("http://www.nyt.com");
-  // await stuff();
+  await stuff();
   res.status(200).json({ name: "Maisey Dog" });
 };
 
