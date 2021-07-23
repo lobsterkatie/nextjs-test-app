fa41755afa0d7d2769853ca09cf146c6345cef92
Katie Byers <lobsterkatie@gmail.com>
2021-07-22 20:34:29 -0700

    ls after build

--- .sentry/TEST.sh
+++ .sentry/TEST.sh
@@ -1,18 +1,15 @@
-# SDK_COMMIT_MESSAGE=$(cd sentry-javascript && git log --format="%C(auto)%s" | head -n 1)
-
-# CONFIGURE_SCOPE_CODE="
-# Sentry.configureScope(scope => {
-#   if (process.env.VERCEL) {
-#     scope.setTag('vercel', true);
-#     scope.setTag('commitMessage', process.env.VERCEL_GIT_COMMIT_MESSAGE);
-#     scope.setTag('sdkCommitMessage', $SDK_COMMIT_MESSAGE);
-#   }
-# });"
-
-# cat .sentry/configurescope.txt >>blah.txt
-# echo "$CONFIGURE_SCOPE_CODE" >>blah.txt
-# echo $CONFIGURE_SCOPE_CODE >>sentry.server.config.js
-# echo $CONFIGURE_SCOPE_CODE >>sentry.client.config.js
-
-# cat sentry.server.config.js
-# cat sentry.client.config.js
+echo " "
+echo "ls"
+ls
+echo " "
+echo "ls .next"
+ls .next
+echo " "
+echo "ls .next/server"
+ls .next/server
+echo " "
+echo "ls .next/serverless"
+ls .next/serverless
+echo " "
+echo "ls static"
+ls static
