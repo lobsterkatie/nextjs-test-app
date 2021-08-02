22575f6c72af8628a59f6e77f9f0265e73b4890d
Katie Byers <lobsterkatie@gmail.com>
2021-08-02 10:13:04 -0700

    don't die if ls directories don't exist

--- .sentry/TEST.sh
+++ .sentry/TEST.sh
@@ -6,10 +6,10 @@ echo "ls .next"
 ls .next
 echo " "
 echo "ls .next/server"
-ls .next/server
+ls .next/server || 0
 echo " "
 echo "ls .next/serverless"
-ls .next/serverless
+ls .next/serverless || 0
 echo " "
-echo "ls static"
-ls static
+echo "ls .next/static"
+ls .next/static || 0
