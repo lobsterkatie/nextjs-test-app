cb9ad28ab10e1519d1bf92230204d92c65f00417
Katie Byers <lobsterkatie@gmail.com>
2021-09-01 19:24:09 -0700

    turn debug back on for server

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -12,7 +12,7 @@ Error.stackTraceLimit = Infinity;
 // debugger;
 Sentry.init({
   dsn: "https://c3d3206b71704aebbe717d5d4dbfe0bd@o87286.ingest.sentry.io/5397699",
-  // debug: true,
+  debug: true,
   // release: "off.leash.trail",
   tracesSampleRate: 1,
   autoSessionTracking: false,
