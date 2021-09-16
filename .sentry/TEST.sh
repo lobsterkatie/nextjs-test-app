bb9fd07524da7351419e2c391281f6621e8c638c
Katie Byers <lobsterkatie@gmail.com>
2021-09-15 21:32:41 -0700

    debug moving graphs, again

--- .sentry/TEST.sh
+++ .sentry/TEST.sh
@@ -14,7 +14,12 @@
 # echo "ls .next/static"
 # ls .next/static || exit 0
 
+ls .next
+[[ -e .next/analyze ]]
+echo $?
+
 # "e" for exists
 if [[ -e .next/analyze ]]; then
+  echo "Moving bundle analysis graphs to $(/public)"
   mv .next/analyze/* public
 fi
