b0385370ba64104d36fa75f06619761cd8cc828e
Katie Byers <lobsterkatie@gmail.com>
2021-10-04 08:47:52 -0700

    add more spots to throw error in test3

--- pages/api/boring/throwError.js
+++ pages/api/boring/throwError.js
@@ -1,13 +1,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 const handler = async (req, res) => {
+  throw new Error("thrown in /throwError API route first thing");
   Sentry.configureScope((scope) => {
     scope.setTag("configureScope", "throwError");
   });
+  throw new Error("thrown in /throwError API route before awaiting the fetch");
   await fetch("http://www.nyt.com");
   Sentry.withScope((scope) => {
     Sentry.setTag("withScope", "withScopeThrowError");
-    throw new Error("thrown in /throwError API route");
+    throw new Error("thrown in /throwError API route after awaiting the fetch");
     res.status(200).json({ name: "Maisey Dog" });
   });
 };
