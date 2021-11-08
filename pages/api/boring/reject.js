7cee00e4f97951a3e1bca511f840c2047203d341
Katie Byers <lobsterkatie@gmail.com>
2021-11-08 06:56:12 -0800

    actually return rejected promise from API route

--- pages/api/boring/reject.js
+++ pages/api/boring/reject.js
@@ -2,7 +2,8 @@ import * as Sentry from "@sentry/nextjs";
 
 const handler = async (req, res) => {
   await fetch("http://www.nyt.com");
-  Promise.reject(new Error("promise rejected in handler"));
+  debugger;
+  return Promise.reject(new Error("promise rejected in handler"));
   res.status(200).json({ name: "Maisey Dog" });
 };
 
