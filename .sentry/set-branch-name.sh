c23c37619cf260751ddd4d7f37eec19d4f02c893
Katie Byers <lobsterkatie@gmail.com>
2021-12-09 11:17:14 -0800

    switch to kmclb-nextjs-remove-resolve-alias-false branch

--- .sentry/set-branch-name.sh
+++ .sentry/set-branch-name.sh
@@ -1,3 +1,4 @@
 # export BRANCH_NAME=kmclb-nextjs-fix-api-error-propagation
 # export BRANCH_NAME=temp-10-8-vercel-testing
-export BRANCH_NAME=kmclb-nextjs-no-instrumentserver-on-vercel
+# export BRANCH_NAME=kmclb-nextjs-no-instrumentserver-on-vercel
+export BRANCH_NAME=kmclb-nextjs-remove-resolve-alias-false
