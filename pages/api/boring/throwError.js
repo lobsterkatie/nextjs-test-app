1675b64d2849b22200aecdf78a984f9e7e88b84d
Katie Byers <lobsterkatie@gmail.com>
2022-02-07 14:23:15 -0800

    shorten error stacktrace in /throwerror

--- pages/api/boring/throwError.js
+++ pages/api/boring/throwError.js
@@ -1,12 +1,22 @@
 import * as Sentry from "@sentry/nextjs";
 
+let x = 0;
+
 const work = () => {
+  x += 1;
+  if (x % 2 === 0) {
+    throw new String("string thrown inside of the work function");
+  } else {
+    throw "string thrown inside of the work function";
+  }
   throw new Error("thrown inside of the work function");
 };
 
 const handler = async (req, res) => {
-  work();
-  throw new Error("thrown in /throwError API route first thing");
+  // work();
+  const err = new Error("thrown in /throwError API route first thing");
+  err.stack = err.stack.split("\n").slice(0, 3).join("\n");
+  throw err;
   Sentry.configureScope((scope) => {
     scope.setTag("configureScope", "throwError");
   });
