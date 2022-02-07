b204767f3c85b323d6f94a6bd4b7b1287a14c3aa
Katie Byers <lobsterkatie@gmail.com>
2022-02-07 13:47:11 -0800

    switch branch to kmclb-nextjs-remove-hack-marking-res-finished

--- .sentry/set-branch-name.sh
+++ .sentry/set-branch-name.sh
@@ -1,4 +1,5 @@
 # export BRANCH_NAME=kmclb-nextjs-fix-api-error-propagation
 # export BRANCH_NAME=temp-10-8-vercel-testing
 # export BRANCH_NAME=kmclb-nextjs-no-instrumentserver-on-vercel
-export BRANCH_NAME=kmclb-nextjs-remove-resolve-alias-false
+# export BRANCH_NAME=kmclb-nextjs-remove-resolve-alias-false
+export BRANCH_NAME=kmclb-nextjs-remove-hack-marking-res-finished
