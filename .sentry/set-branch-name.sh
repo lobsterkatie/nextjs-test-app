59d71a38b1913a104d12c1c3920ebcd4b921bcd5
Katie Byers <lobsterkatie@gmail.com>
2022-04-25 08:29:32 -0700

    change to using 4-25 branch

--- .sentry/set-branch-name.sh
+++ .sentry/set-branch-name.sh
@@ -2,4 +2,4 @@
 # export BRANCH_NAME=temp-10-8-vercel-testing
 # export BRANCH_NAME=kmclb-nextjs-no-instrumentserver-on-vercel
 # export BRANCH_NAME=kmclb-nextjs-remove-resolve-alias-false
-export BRANCH_NAME=kmclb-nextjs-remove-hack-marking-res-finished
+export BRANCH_NAME=kmclb-4-25-vercel
