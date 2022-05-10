890bef749316c123c494bd8fbcd9fadf179c87a2
Katie Byers <lobsterkatie@gmail.com>
2022-05-10 08:00:08 -0700

    switch to eval testing branch

--- .sentry/set-branch-name.sh
+++ .sentry/set-branch-name.sh
@@ -2,4 +2,5 @@
 # export BRANCH_NAME=temp-10-8-vercel-testing
 # export BRANCH_NAME=kmclb-nextjs-no-instrumentserver-on-vercel
 # export BRANCH_NAME=kmclb-nextjs-remove-resolve-alias-false
-export BRANCH_NAME=kmclb-4-25-vercel
+# export BRANCH_NAME=kmclb-4-25-vercel
+export BRANCH_NAME=kmclb-fix-eval-serverless-express-import-warnings
